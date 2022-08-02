0120925b7af460123bc94d4e6b8b7fe5405adf44
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-08-02 15:04:09 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,18 @@
+# Projeto Pokémon Slider
+
+## Descrição
+Projeto realizado durante o evento da MapaDev Week. Realizadas apenas umas modificações para que o slider fique de forma infinita
+
+# Tecnologias
+- HTML
+- CSS
+- JS
+
+# Screenshots
+
+### Desktop
+![](./design/desktop.png)
+
+### Mobile
+
+![](./design/mobile.png)
---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
+}
+
+btnAvancar.addEventListener("click", function () {
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+
+  if (cartaoAtual >= cartoes.length) {
+    cartaoAtual = 0
+  }
+
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+
+  if (cartaoAtual === 0){
+    cartaoAtual = cartoes.length
+  };
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
